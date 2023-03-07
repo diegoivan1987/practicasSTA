72de97df74c8b74f47a8396cbbe9cbc79a12a76f
Diego Ivan <diegoivan1987@outlook.com>
2023-03-06 23:24:42 -0600

    se aniadio el pdf y se borraron docs inecesarios

--- main.py
+++ main.py
@@ -1,135 +1,39 @@
-from random import randint
-from PyQt5 import QtCore, QtWidgets
-from PyQt5 import uic
-import sys, time
+import requests
+import json
+import datetime
 
-class HiloPrincipal(QtCore.QThread):
+from prefect import task, Flow
+from prefect.schedules import IntervalSchedule
 
-	porcentaje = QtCore.pyqtSignal(int)
-	fila = QtCore.pyqtSignal(int)
-	idProceso = QtCore.pyqtSignal(dict)
-	cambiarLabel = QtCore.pyqtSignal(int)
+## extract
+@task(cache_for=datetime.timedelta(days=1))
+def get_price():
+    r = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=mxn")
+    response_json = json.loads(r.text)
+    return response_json
 
-	def __init__(self, error, cola):
-		super(HiloPrincipal, self).__init__(None)
-		self.error = error
-		self.cola = cola
+## transform
+@task
+def parse_data(raw):
+    price = raw['dogecoin']['mxn']
+    return price
 
-	#proceso principal
-	def run(self):
-		while True:
-			if self.error[0] == 0:
-				self.procesamiento(self.cola,self.error)
+## load
+@task
+def send_notificacion(parsed):
+    token = "6191852587:AAEyCD59d37jirpOB2d0Zq-wMiTLBjCKjfo"
+    url = f"https://api.telegram.org/bot{token}"
+    if parsed < 1:
+        params = {"chat_id": "5473415339", "text": "Compraaaa!!!!"}
+        r = requests.get(url + "/sendMessage", params=params)
+    elif parsed > 2.5:
+        params = {"chat_id": "5473415339", "text": "vende!!!!"}
+        r = requests.get(url + "/sendMessage", params=params)
+schedule = IntervalSchedule(interval=datetime.timedelta(seconds=10))
 
-	def procesamiento(self,cola,error):
-		try:
-			if len(cola)>0:
-				probabilidad = randint(1,3)
-				if probabilidad ==1:
-					hola = 1/0
-				else:
-					for i in range (1,101):
-						self.porcentaje.emit(i)
-						time.sleep(0.01)
-					id = cola.pop(0)
-					self.fila.emit(1)
-					self.idProceso.emit({"senial":1,"id":id})
-					time.sleep(0.01)
-		except:
-			error[0] = 1
-			time.sleep(0.01)
-			self.cambiarLabel.emit(1)
-			time.sleep(0.01)
-			
-			
+with Flow("my etl flow",schedule) as f:
+    raw = get_price()
+    parsed = parse_data(raw)
+    send_notificacion(parsed)
 
-class HiloSecundario(QtCore.QThread):
-	porcentaje = QtCore.pyqtSignal(int)
-	fila = QtCore.pyqtSignal(int)
-	idProceso = QtCore.pyqtSignal(dict)
-
-	def __init__(self, error, cola):
-		super(HiloSecundario, self).__init__(None)
-		self.error = error
-		self.cola = cola
-
-	#proceso principal
-	def run(self):
-		while True:
-			if self.error[0] == 1:
-				self.procesamiento(self.cola)
-
-	def procesamiento(self,cola):
-		if len(cola)>0:
-			for i in range (1,101):
-				self.porcentaje.emit(i)
-				time.sleep(0.01)
-			id = cola.pop(0)
-			self.fila.emit(1)
-			self.idProceso.emit({"senial":1,"id":id})
-			time.sleep(0.01)
-		
-
-	
-class VentanaPrincipal(QtWidgets.QMainWindow):
-	def __init__(self):
-		QtWidgets.QMainWindow.__init__(self)
-		self.ui = uic.loadUi('prueba.ui',self)#Se carga la interfaz grafica
-
-		self.procesosPendientes = []
-		self.error = [0]
-		#inicializamos los arreglos de procesos y sus datos
-		for i in range(10):
-			self.procesosPendientes.append(i+1)
-
-
-		#llenamos la tabla de pendientes
-		for i in range(len(self.procesosPendientes)):
-			self.tablaPendientes.insertRow(self.tablaPendientes.rowCount())
-			id = QtWidgets.QTableWidgetItem(str(self.procesosPendientes[i]))
-			self.tablaPendientes.setItem(i,0,id)
-
-		self.btnI.clicked.connect(self.correr)
-
-
-	def correr(self):
-		self.hiloPrincipal = HiloPrincipal(self.error,self.procesosPendientes)
-		self.hiloPrincipal.porcentaje.connect(self.socketPorcentaje)
-		self.hiloPrincipal.fila.connect(self.socketFila)
-		self.hiloPrincipal.idProceso.connect(self.socketIdProceso)
-		self.hiloPrincipal.cambiarLabel.connect(self.socketCambiarLabel)
-
-		self.hiloSecundario = HiloSecundario(self.error,self.procesosPendientes)
-		self.hiloSecundario.porcentaje.connect(self.socketPorcentajeSecundario)
-		self.hiloSecundario.fila.connect(self.socketFila)
-		self.hiloSecundario.idProceso.connect(self.socketIdProceso)
-
-		self.hiloPrincipal.start()
-		self.hiloSecundario.start()
-	
-	def socketPorcentaje(self,senial):
-		self.barra_1.setValue(senial)
-
-	def socketCambiarLabel(self,senial):
-		if senial == 1:
-			self.lbError.setText("Error al ejecutar tarea")
-			self.barra_1.setValue(0)
-
-	def socketPorcentajeSecundario(self,senial):
-		self.barra_2.setValue(senial)
-
-	def socketFila(self,senial):
-		if senial == 1:
-			self.tablaPendientes.removeRow(0)
-
-	def socketIdProceso(self,senial):
-		if senial["senial"] == 1:
-			self.tablaTerminados.insertRow(self.tablaTerminados.rowCount())
-			id = QtWidgets.QTableWidgetItem(str(senial["id"]))
-			self.tablaTerminados.setItem(self.tablaTerminados.rowCount()-1,0,id)
-
-#Iniciamos la aplicacion en bucle
-app = QtWidgets.QApplication(sys.argv)
-mainWindow = VentanaPrincipal()
-mainWindow.show()
-sys.exit(app.exec_())
+f.run()
